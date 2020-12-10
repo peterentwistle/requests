601d15cbacca374fe8f86d7e74cf72b2ba5d7bdd
Peter <petertheanimator@gmail.com>
2020-12-10 18:48:40 +0000

    Added bearer authentication support

--- Sources/Requests/Requests.swift
+++ Sources/Requests/Requests.swift
@@ -34,57 +34,67 @@ public protocol Requestable {
     ///
     /// - Parameters:
     ///     - url: The String representation of the URL to make the request to.
+    ///     - authentication: The authentication to be applied to the request
     ///     - completionHandler: The completion handler to call when the request has completed.
-    static func get(_ url: String, completionHandler: @escaping (Response) -> ())
+    static func get(_ url: String, authentication: Authentication?, completionHandler: @escaping (Response) -> ())
     
     /// Makes a POST request
     ///
     /// - Parameters:
     ///     - url: The String representation of the URL to make the request to.
     ///     - data: The data to pass in the body of the request.
+    ///     - authentication: The authentication to be applied to the request
     ///     - completionHandler: The completion handler to call when the request has completed.
-    static func post(_ url: String, data: [String: String]?, completionHandler: @escaping (Response) -> ())
+    static func post(_ url: String, data: [String: String]?, authentication: Authentication?, completionHandler: @escaping (Response) -> ())
     
     /// Makes a POST request
     ///
     /// - Parameters:
     ///     - url: The String representation of the URL to make the request to.
     ///     - json: The json data to pass in the body of the request.
+    ///     - authentication: The authentication to be applied to the request
     ///     - completionHandler: The completion handler to call when the request has completed.
-    static func post(_ url: String, json: Data?, completionHandler: @escaping (Response) -> ())
+    static func post(_ url: String, json: Data?, authentication: Authentication?, completionHandler: @escaping (Response) -> ())
     
     /// Makes a PUT request
     ///
     /// - Parameters:
     ///     - url: The String representation of the URL to make the request to.
     ///     - data: The data to pass in the body of the request.
+    ///     - authentication: The authentication to be applied to the request
     ///     - completionHandler: The completion handler to call when the request has completed.
-    static func put(_ url: String, data: [String: String]?, completionHandler: @escaping (Response) -> ())
+    static func put(_ url: String, data: [String: String]?, authentication: Authentication?, completionHandler: @escaping (Response) -> ())
     
     /// Makes a PATCH request
     ///
     /// - Parameters:
     ///     - url: The String representation of the URL to make the request to.
     ///     - data: The data to pass in the body of the request.
+    ///     - authentication: The authentication to be applied to the request
     ///     - completionHandler: The completion handler to call when the request has completed.
-    static func patch(_ url: String, data: [String: String]?, completionHandler: @escaping (Response) -> ())
+    static func patch(_ url: String, data: [String: String]?, authentication: Authentication?, completionHandler: @escaping (Response) -> ())
     
     /// Makes a DELETE request
     ///
     /// - Parameters:
     ///     - url: The String representation of the URL to make the request to.
     ///     - data: The data to pass in the body of the request.
+    ///     - authentication: The authentication to be applied to the request
     ///     - completionHandler: The completion handler to call when the request has completed.
-    static func delete(_ url: String, data: [String: String]?, completionHandler: @escaping (Response) -> ())
+    static func delete(_ url: String, data: [String: String]?, authentication: Authentication?, completionHandler: @escaping (Response) -> ())
 }
 
 extension Requestable {
-    private static func make(method: HttpMethod, url: String, data: [String: String]? = nil, json: Data? = nil, completionHandler: @escaping (Response) -> ()) {
+    private static func make(method: HttpMethod, url: String, data: [String: String]? = nil, json: Data? = nil, authentication: Authentication? = nil, completionHandler: @escaping (Response) -> ()) {
         var request = URLRequest(url: URL(string: url)!)
         request.httpMethod = method.rawValue
         
         if let data = data {
             request.httpBody = data.bodyData()
+        }
+        
+        if let authentication = authentication {
+            request.addAuthHeaders(authentication: authentication)
         }
         
         if let json = json {
@@ -99,38 +109,38 @@ extension Requestable {
         }.resume()
     }
     
-    public static func get(_ url: String, completionHandler: @escaping (Response) -> ()) {
-        make(method: .get, url: url, data: nil) { response in
+    public static func get(_ url: String, authentication: Authentication? = nil, completionHandler: @escaping (Response) -> ()) {
+        make(method: .get, url: url, data: nil, authentication: authentication) { response in
             completionHandler(response)
         }
     }
     
-    public static func post(_ url: String, data: [String : String]? = nil, completionHandler: @escaping (Response) -> ()) {
-        make(method: .post, url: url, data: data) { response in
+    public static func post(_ url: String, data: [String : String]? = nil, authentication: Authentication? = nil, completionHandler: @escaping (Response) -> ()) {
+        make(method: .post, url: url, data: data, authentication: authentication) { response in
             completionHandler(response)
         }
     }
     
-    public static func post(_ url: String, json: Data?, completionHandler: @escaping (Response) -> ()) {
-        make(method: .post, url: url, json: json) { response in
+    public static func post(_ url: String, json: Data?, authentication: Authentication? = nil, completionHandler: @escaping (Response) -> ()) {
+        make(method: .post, url: url, json: json, authentication: authentication) { response in
             completionHandler(response)
         }
     }
     
-    public static func put(_ url: String, data: [String : String]? = nil, completionHandler: @escaping (Response) -> ()) {
-        make(method: .put, url: url, data: data) { response in
+    public static func put(_ url: String, data: [String : String]? = nil, authentication: Authentication? = nil, completionHandler: @escaping (Response) -> ()) {
+        make(method: .put, url: url, data: data, authentication: authentication) { response in
             completionHandler(response)
         }
     }
     
-    public static func patch(_ url: String, data: [String : String]? = nil, completionHandler: @escaping (Response) -> ()) {
-        make(method: .patch, url: url, data: data) { response in
+    public static func patch(_ url: String, data: [String : String]? = nil, authentication: Authentication? = nil, completionHandler: @escaping (Response) -> ()) {
+        make(method: .patch, url: url, data: data, authentication: authentication) { response in
             completionHandler(response)
         }
     }
     
-    public static func delete(_ url: String, data: [String : String]? = nil, completionHandler: @escaping (Response) -> ()) {
-        make(method: .delete, url: url, data: data) { response in
+    public static func delete(_ url: String, data: [String : String]? = nil, authentication: Authentication? = nil, completionHandler: @escaping (Response) -> ()) {
+        make(method: .delete, url: url, data: data, authentication: authentication) { response in
             completionHandler(response)
         }
     }
@@ -148,5 +158,12 @@ extension Dictionary where Key == String, Value == String {
         }
         
         return data.joined(separator: "&").data(using: .utf8)!
+    }
+}
+
+// MARK: - addAuthHeaders
+extension URLRequest {
+    mutating func addAuthHeaders(authentication: Authentication) {
+        self.addValue(authentication.authorizationHeader, forHTTPHeaderField: "Authorization")
     }
 }
